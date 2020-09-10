0aa93be6ce1b844c564c656808c0daec993ff97d
Wilder Rincon <1588@holbertonschool.com>
2020-09-10 17:58:27 -0500

    Adding changes files app.py index.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
-"""init flask"""
-from flask import Flask, jsonify
+"""app.py to connect to API"""
+import os
 from models import storage
 from api.v1.views import app_views
-from os import getenv
+from flask import Flask, Blueprint, jsonify, make_response
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_close(db_close):
-    """close the ssesion"""
+def teardown_appcontext(code):
+    """teardown_appcontext"""
     storage.close()
 
 
 @app.errorhandler(404)
-def resource_not_found(e):
-    """return error 404"""
-    return jsonify({
-                    "error": "Not found" 
-                  }), 404
+def page_not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
-
-if __name__ == '__main__':
-    host = getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(getenv("HBNB_API_PORT", "5000"))
-    app.url_map.strict_slashes = False
-    app.run(host, port, threaded=True)
+if __name__ == "__main__":
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
